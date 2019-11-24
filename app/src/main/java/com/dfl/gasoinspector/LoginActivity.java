beb6b733b06a096b1237a52cf0a090af0be16308
dellylucas <dellylucas@hotmail.com>
2019-11-23 22:52:22 -0500

    finalizacion login

---
+++ app/src/main/java/com/dfl/gasoinspector/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.dfl.gasoinspector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent in = new Intent(this, DeviceListActivity.class);
+        startActivity(in);
+    }
+}
